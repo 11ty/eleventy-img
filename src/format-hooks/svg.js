f7866c35c0e9ed82bda845f6f4d70c107753db44
Zach Leatherman <zachleat@users.noreply.github.com>
2024-10-01 14:48:45 -0500

    Merge upstream changes

--- src/format-hooks/svg.js
+++ src/format-hooks/svg.js
@@ -3,16 +3,8 @@ const fsp = require("fs").promises;
 module.exports = async function createSvg(sharpInstance) {
   let input = sharpInstance.options.input;
   let svgBuffer = input.buffer;
-
   if(svgBuffer) { // remote URL already has buffer
-    let svgString = svgBuffer.toString("utf-8")
-
-    if(!svgString.includes('<svg')) {
-      // based on https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder#examples
-      let utf8decoder = new TextDecoder()
-      svgString = utf8decoder.decode(svgBuffer)
-    }
-    return svgString
+    return svgBuffer;
   } else { // local file system
     return fsp.readFile(input.file);
   }
