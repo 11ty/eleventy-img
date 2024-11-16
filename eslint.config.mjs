de5fc4d691449940bb2bd0cf838658288e1a0575
Zach Leatherman <zachleat@users.noreply.github.com>
2024-11-16 14:20:38 -0600

    Upgrade eslint to latest

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+  },
+
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
+  },
+}];
